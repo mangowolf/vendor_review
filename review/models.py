ac46e2f46afe8700dc6a59cf61cf8bca9ffbc7a0
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-09-27 21:58:06 -0700

    Type: Database Created Person model with username, first, and last name.

--- review/models.py
+++ review/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    username = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
