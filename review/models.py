a84d0b882e80b32fbf0e52e99df8626b6a6c7e83
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-09-30 08:35:28 -0700

    Type: CSS Reduced star size, removed indent, and padding that was clipping the right point of the star.

--- review/models.py
+++ review/models.py
@@ -6,5 +6,21 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     username = models.CharField(max_length=30)
+    email = models.EmailField()
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class Company(models.Model):
+    website = models.URLField()
+    headquarters = models.CharField(max_length=255)
+    industry = models.CharField(max_length=255)
+    revenue = models.IntegerField()
+    size = models.IntegerField()
+    founded = models.DateField()
+    summary = models.TextField()
+
+class Company_Reviews(models.Model):
+    companykey = models.ForeignKey(
+        'Company',
+        on_delete=models.CASCADE,
+        )
