5f11a7fea8418f8ea95bfa58d65d81dcd84996ef
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-10-17 21:57:16 -0700

    Type: Test Updated jquery for multiple test scripts.

--- review/static/review/js/review.js
+++ review/static/review/js/review.js
@@ -1,12 +1,14 @@
 /*REVIEWS JAVASCRIPT FILE*/
 
 $(document).ready(function(){
-    $("div[name=Ad").click(function(){
-        $("this").hide();
+    $("div[name='Ad']").click(function(){
+        $(this).hide();
     });
+});
 
+$(document).ready(function(){
     var count = 1;
     $("button").click(function(){
         $("#counter").text(count++);
-    })
+    });
 });
