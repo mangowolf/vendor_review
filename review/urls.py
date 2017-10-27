0a1b500db012d5fdb9e7df82d7ae38de442c0091
Jimmy Nguyen <jimmyz949@hotmail.com>
2017-10-26 21:16:12 -0700

    Type:Feature Added ability to edit a post if logged in as authenticated User.

--- review/urls.py
+++ review/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^new_company_review/$', views.new_company_review, name='new_company_review'),
     url(r'^company_review/$', views.company_review, name='company_review'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- review/views.py
+++ review/views.py
@@ -29,6 +29,20 @@ def new_company_review(request):
         form = PostForm()
     return render(request, 'review/new_company_review.html', {'form': form})
 
+def post_edit(request, pk):
+    post = get_object_or_404(Company_Reviews, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'review/new_company_review.html', {'form': form})
+
 def company_review2(request):
     parsedData = []
     if request.method == 'POST':
